07f775e7d2ac8c0b519e23d6769717721548726a
frankluongo <frank@frankluongo.com>
2021-10-12 18:54:26 -0400

    make some quick fixes!

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,8 +2,15 @@ function Buttons() {
   const buttons = document.querySelectorAll('[data-button]');
   const continueBtn = document.querySelector('[data-continue]');
 
-  function onBtnClick() {
+  function resetButtons() {
+    buttons.forEach((btn) => btn.classList.remove('active'));
+  }
+
+  function onBtnClick(e) {
     continueBtn.removeAttribute('disabled');
+    resetButtons();
+    e.target.classList.add('active');
+    continueBtn.href = e.target.dataset.link;
   }
 
   buttons.forEach((button) => {
